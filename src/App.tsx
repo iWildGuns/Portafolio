3c51b1e7ee0dd91c5087b5b8ad6ed237e0dee0c7
Hector Diaz <hrdr1995@gmail.com>
2026-04-20 20:27:35 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,9 @@ function App() {
           <p className="container px-auto text-white sm:text-2xl indent-4 ">
             Bienvenido a mi portafolio. Soy un desarrollador backend junior
             especializado en tecnologías como Node.js y JavaScript, con
-            experiencia en bases de datos relacionales y no relacionales, así
-            como en la creación de API. Cada proyecto aquí refleja mi dedicación
+            experiencia en <b>Base de datos</b> relacionales y no relacionales, así
+            como en la creación de APIs REST. Cada proyecto aquí refleja mi dedicación
             por la calidad del código y mi constante búsqueda de aprendizaje.
-            Explora mis trabajos para ver cómo aplico mis conocimientos técnicos
-            y mi enfoque proactivo en la resolución de problemas.
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 py-10">
