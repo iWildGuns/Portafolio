a54579e05ab722c88166136ecaa29672083a35bc
Hector Diaz <hrdr1995@gmail.com>
2026-09-23 14:42:29 -0300

    acomodando

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FaEnvelope, FaWhatsapp, FaPaperPlane } from "react-icons/fa";
+import { FaEnvelope, FaWhatsapp } from "react-icons/fa";
 
 export default function Contact() {
   return (
